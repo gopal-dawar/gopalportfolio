0d16d7a10eee1d8d08eb1116a1280f9720f04c5e
Gopal Dawar <gopaldawar45@gmail.com>
2025-01-16 23:13:22 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function() {
+    const header = document.getElementById('main-header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
